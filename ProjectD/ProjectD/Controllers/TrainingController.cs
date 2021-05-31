40edb41590d32d13dd6093d0bd99f8126e0e4fd8
Audrynass <55562216+AudriusPaulaitis@users.noreply.github.com>
2021-05-31 19:43:06 +0200

    opzet

---
+++ ProjectD/ProjectD/Controllers/TrainingController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectD.Controllers
+{
+    public class TrainingController : Controller
+    {
+        public List<string> TrainingList { get; set; }
+        public TrainingController()
+        {
+            TrainingList = new List<string>();
+        }
+        public IActionResult Trainingen()
+        {
+            int indexN = 0;
+            switch (indexN)
+            {
+                case 0:
+                    TrainingList.Add("training 1");
+                    TrainingList.Add("training 4");
+                    TrainingList.Add("training 5");
+                    break;
+                case 1:
+                    TrainingList.Add("training 2");
+                    TrainingList.Add("training 3");
+                    break;
+            }
+            return View(TrainingList);
+        }
+    }
+}
---
+++ ProjectD/ProjectD/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectD.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public float Height { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
