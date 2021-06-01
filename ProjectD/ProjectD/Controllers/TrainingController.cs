71324c32b2f19b80ba6b38c3c4e9e1bb57cb172c
Audrynass <55562216+AudriusPaulaitis@users.noreply.github.com>
2021-06-01 23:30:02 +0200

    trainingen

--- ProjectD/ProjectD/Controllers/TrainingController.cs
+++ ProjectD/ProjectD/Controllers/TrainingController.cs
@@ -18,19 +18,22 @@ namespace ProjectD.Controllers
         public IActionResult Trainingen(int TrainingFrequency, int WeeklyGoal, int Goal)
         {
             int TF = TrainingFrequency;
+            var wg = (double)WeeklyGoal;
             training.Weeks = (Goal / WeeklyGoal);
             for (int w = 1; w <= training.Weeks; w++)
             {
                 var TrainingList = new List<string>();
-                TrainingList.Add($"Normale training {WeeklyGoal / 2} KM");
+                TrainingList.Add($"Duurloop training {Math.Round(wg / 2, 1)} KM");
                 while (TrainingFrequency - 1 != 0)
                 {
-                    TrainingList.Add($"Normale training {WeeklyGoal / 2} KM");
+                    TrainingList.Add($"Normale training {Math.Round(wg / 2, 1)} KM");
                     TrainingFrequency -= 1;
                 }
+                wg *= 1.1;
                 TrainingFrequency = TF;
                 training.TrainingDict.Add($"Week {w}", TrainingList);
             }
+            
             return View(training);
         }
     }
