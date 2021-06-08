bf6a0bc5f3239e62f0c74af51f1917a0e5cbd6c0
Jacco101 <jaccoburghouwt@hotmail.com>
2021-06-08 21:13:41 +0200

    Overbodige data verwijderd
    lees titel

--- ProjectD/ProjectD/ApiCaller.cs
+++ ProjectD/ProjectD/ApiCaller.cs
@@ -48,25 +48,5 @@ namespace ProjectD
                 return result;
             }
         }
-
-        //niet af
-        public static string GetActivitySummary()
-        {
-            //string date = DateTime.Now.ToString("yyyy-MM-dd");
-            var url = $@"https://api.fitbit.com/1/user/-/profile.json";
-
-            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
-
-            httpRequest.Accept = "application/json";
-            httpRequest.Headers["Authorization"] = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyM0IyN1oiLCJzdWIiOiI5RkNDMjIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd3BybyB3bnV0IHdzbGUgd3dlaSB3c29jIHdzZXQgd2FjdCB3bG9jIiwiZXhwIjoxNjIzNzc4NTQ2LCJpYXQiOjE2MjMxNzM3NDZ9.88ptapYGRfdtABlLKrHAbdinHeJzfiuCr-MZDt1Bn4M";
-
-
-            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
-            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
-            {
-                var result = streamReader.ReadToEnd();
-                return result;
-            }
-        }
     }
 }
